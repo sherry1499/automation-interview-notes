a10ae0ea44d0793d9bca2628b257ed5c257e72a3
sherry1499 <1499836630@qq.com>
2026-02-12 17:31:15 +0800

    修复Jenkinsfile使用虚拟环境

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,13 +10,20 @@ pipeline {
 
         stage('安装依赖') {
             steps {
-                sh 'pip install pytest'
+                sh '''
+                    python3 -m venv venv
+                    . venv/bin/activate
+                    pip install pytest
+                '''
             }
         }
 
         stage('运行测试') {
             steps {
-                sh 'pytest test_demo.py -v'
+                sh '''
+                    . venv/bin/activate
+                    pytest test_demo.py -v
+                '''
             }
         }
     }
